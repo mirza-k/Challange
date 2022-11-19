cf8072db391a657cba3d5962f0a5201bed3bc04a
Mirza Kozica <mirza.kozica@authoritypartners.com>
2022-11-19 18:05:52 +0100

    Refactoring code in controller to use service method.

--- BackendCandidateChallenge/QuizService/Controllers/QuizController.cs
+++ BackendCandidateChallenge/QuizService/Controllers/QuizController.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Data;
-using Dapper;
+﻿using Dapper;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 using QuizModel.Domain;
 using QuizModel.Model;
+using QuizServices.Interfaces;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace QuizService.Controllers;
 
@@ -12,10 +13,12 @@ namespace QuizService.Controllers;
 public class QuizController : Controller
 {
     private readonly IDbConnection _connection;
+    private IQuizService _quizService;
 
-    public QuizController(IDbConnection connection)
+    public QuizController(IDbConnection connection, IQuizService quizService)
     {
         _connection = connection;
+        _quizService = quizService;
     }
 
     // GET api/quizzes
@@ -36,43 +39,10 @@ public class QuizController : Controller
     [HttpGet("{id}")]
     public object Get(int id)
     {
-        const string quizSql = "SELECT * FROM Quiz WHERE Id = @Id;";
-        var quiz = _connection.QuerySingleOrDefault<Quiz>(quizSql, new {Id = id});
+        var quiz = _quizService.GetById(id);
         if (quiz == null)
             return NotFound();
-        const string questionsSql = "SELECT * FROM Question WHERE QuizId = @QuizId;";
-        var questions = _connection.Query<Question>(questionsSql, new {QuizId = id});
-        const string answersSql = "SELECT a.Id, a.Text, a.QuestionId FROM Answer a INNER JOIN Question q ON a.QuestionId = q.Id WHERE q.QuizId = @QuizId;";
-        var answers = _connection.Query<Answer>(answersSql, new {QuizId = id})
-            .Aggregate(new Dictionary<int, IList<Answer>>(), (dict, answer) => {
-                if (!dict.ContainsKey(answer.QuestionId))
-                    dict.Add(answer.QuestionId, new List<Answer>());
-                dict[answer.QuestionId].Add(answer);
-                return dict;
-            });
-        return new QuizResponseModel
-        {
-            Id = quiz.Id,
-            Title = quiz.Title,
-            Questions = questions.Select(question => new QuizResponseModel.QuestionItem
-            {
-                Id = question.Id,
-                Text = question.Text,
-                Answers = answers.ContainsKey(question.Id)
-                    ? answers[question.Id].Select(answer => new QuizResponseModel.AnswerItem
-                    {
-                        Id = answer.Id,
-                        Text = answer.Text
-                    })
-                    : new QuizResponseModel.AnswerItem[0],
-                CorrectAnswerId = question.CorrectAnswerId
-            }),
-            Links = new Dictionary<string, string>
-            {
-                {"self", $"/api/quizzes/{id}"},
-                {"questions", $"/api/quizzes/{id}/questions"}
-            }
-        };
+        return quiz;
     }
 
     // POST api/quizzes
